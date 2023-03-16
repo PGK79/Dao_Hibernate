e84cf30eb5ff266a0544d0b7298390ff220a7f4b
PGK79 <pgk_nev@mail.ru>
2023-03-16 23:23:35 +0400

    Добавлены DaoWithHibernateApplication.java

---
+++ src/main/java/com/example/dao_with_hibernate/DaoWithHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.example.dao_with_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoWithHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoWithHibernateApplication.class, args);
+    }
+}
