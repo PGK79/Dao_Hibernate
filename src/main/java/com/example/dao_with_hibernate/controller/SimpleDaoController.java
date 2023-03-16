d8977b402faebe447c5ea158e79e1734c762530d
PGK79 <pgk_nev@mail.ru>
2023-03-16 23:24:09 +0400

    Добавлены контроллер

---
+++ src/main/java/com/example/dao_with_hibernate/controller/SimpleDaoController.java
@@ -0,0 +1,23 @@
+package com.example.dao_with_hibernate.controller;
+
+import com.example.dao_with_hibernate.entity.Person;
+import com.example.dao_with_hibernate.repository.SimpleDaoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SimpleDaoController {
+    private final SimpleDaoRepository simpleDaoRepository;
+
+    public SimpleDaoController(SimpleDaoRepository simpleDaoRepository) {
+        this.simpleDaoRepository = simpleDaoRepository;
+    }
+
+    @GetMapping("/person/by-city")
+    public List<Person> getPersons(@RequestParam("city") String city) {
+        return simpleDaoRepository.getPersonByCity(city);
+    }
+}
