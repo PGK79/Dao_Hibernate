38495b76a9025073ed05109f50299dfc8b84a5f0
PGK79 <pgk_nev@mail.ru>
2023-03-17 18:01:51 +0400

    Добавлены поля в новую базу данных

--- src/main/resources/db/changelog/migrations/001-create_table.sql
+++ src/main/resources/db/changelog/migrations/001-create_table.sql
@@ -5,6 +5,9 @@ CREATE TABLE tables_for_task_three.migration_table_hibernate
 (
     id      int         not null auto_increment primary key,
     name    varchar(20) not null,
-    surname varchar(30) not null
+    surname varchar(30) not null,
+    age int not null,
+    phone_number varchar(30) not null
+
 );
 -- rollback DROP TABLE tables_for_task_three.migration_table_hibernate;
