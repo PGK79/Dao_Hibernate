a105478c9ff9179c6d7d5470d537296426b9a2a4
PGK79 <pgk_nev@mail.ru>
2023-03-21 14:53:11 +0400

    Добавлена конфигурация для аутентификации и авторизации

---
+++ src/main/java/com/example/dao_with_hibernate/config/PersonSecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.dao_with_hibernate.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersonSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
+
+    public PersonSecurityConfiguration(@Autowired DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+        //.rolePrefix("ROLE_");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests().antMatchers("/by-city").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/persons/by-age").hasRole("Investigator")
+                .and()
+                .authorizeRequests().antMatchers("/persons/by-fullname").hasRole("Mistress")
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+
+    }
+}
