4c957e446db17305b0f79012487a6c53fdb6d3d7
PGK79 <pgk_nev@mail.ru>
2023-03-25 19:49:47 +0400

    Добавлена аннотация @EnableGlobalMethodSecurity, убран метод configur(http)

--- src/main/java/com/example/dao_with_hibernate/config/PersonSecurityConfiguration.java
+++ src/main/java/com/example/dao_with_hibernate/config/PersonSecurityConfiguration.java
@@ -3,12 +3,16 @@ package com.example.dao_with_hibernate.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
 public class PersonSecurityConfiguration extends WebSecurityConfigurerAdapter { //Deprecated класс по условию задачи
     private final DataSource dataSource;
 
@@ -19,16 +23,5 @@ public class PersonSecurityConfiguration extends WebSecurityConfigurerAdapter { 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasRole("Investigator")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-fullname").hasRole("Mistress")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
     }
 }
